66fa1fbf60616c3e22b7fc32e5a4fef1863d2d46
Tambosa <tambosa@gmail.com>
2023-06-07 17:12:55 +0300

    navigating with args

---
+++ app/src/main/java/com/example/testcatering/ui/category/CategoryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.testcatering.ui.category
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CategoryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is account Fragment"
+    }
+    val text: LiveData<String> = _text
+}
--- app/src/main/java/com/example/testcatering/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/testcatering/ui/home/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.testcatering.R
 import com.example.testcatering.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -46,6 +48,12 @@ class HomeFragment : Fragment() {
     }
 
     private fun onCategoryItemClick(position: Int) {
+        val bundle = Bundle()
+        bundle.putString("category", viewModel.data.value?.get(position)?.name)
+        findNavController().navigate(
+            R.id.navigation_category,
+            bundle
+        )
     }
 
 
